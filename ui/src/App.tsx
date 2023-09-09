8d4f69c6ce3a95f16d466b40874e0b581a83c8ba
nikaidou-haruki <meteo.haru@gmail.com>
2023-09-09 13:37:23 +0100

    add function: set expire time

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,11 +6,12 @@ import Button from "./components/Button/Button";
 import TextField from "./components/TextField/TextField";
 import Select from "./components/Select/Select.tsx";
 
-async function getShortUrl(url: string): Promise<string> {
+async function getShortUrl(url: string, expireTimeIndex: number): Promise<string> {
     const urlBase = window.location.href;
     return new Promise((resolve, reject) => {
         axios.post("/shorten", {
-            originalUrl: url
+            originalUrl: url,
+            expiresAt: requestExpireTimeOptions[expireTimeIndex]
         }).then((response) => {
             resolve(urlBase + response.data["shortPath"]);
         }).catch((error) => {
@@ -26,16 +27,24 @@ const expireTimeOptions = [
     "Never"
 ];
 
+const requestExpireTimeOptions = [
+    "1day",
+    "3days",
+    "week",
+    "never"
+];
+
 export default function App() {
     const [shortUrl, setShortUrl] = createSignal("");
     const [longUrl, setLongUrl] = createSignal("");
+    let expireTimeIndex = 0;
 
     const longUrlChangeListener = (value: string) => {
         setLongUrl(value);
     };
     const onClickShortenButton = async () => {
         try {
-            const shortenUrl = await getShortUrl(longUrl());
+            const shortenUrl = await getShortUrl(longUrl(), expireTimeIndex);
             setShortUrl(shortenUrl);
         } catch (error) {
             console.error(error);
@@ -48,14 +57,18 @@ export default function App() {
         navigator.clipboard.writeText(shortUrl()).then(() => {});
     };
 
+    const onChangeExpireTime = (index: number) => {
+        expireTimeIndex = index;
+    };
+
     return (
         <div class="content">
             <Box width="20em">
                 <div class="column">
                     <h1>Shorten URL</h1>
                     <TextField placeholder="Enter URL Here" onChange={longUrlChangeListener}/>
-                    <h3>Link expiration</h3>
-                    <Select items={expireTimeOptions} />
+                    <h3>Link expiration after</h3>
+                    <Select items={expireTimeOptions} onChange={onChangeExpireTime}/>
                     <Button text="Shorten" onClick={onClickShortenButton} />
                     <TextField placeholder="Shortened URL" readonly value={shortUrl}/>
                     <Button text="Copy" onClick={onClickCopyButton} />
